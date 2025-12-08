8f8c9bea5d2516983448ec001c278458f449f5c5
Tintenklecks <Github@puco.de>
2025-12-08 20:43:14 +0100

    Initial Commit

---
+++ Noise Therapy/Noise_TherapyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Noise_TherapyApp.swift
+//  Noise Therapy
+//
+//  Created by puco on 08.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Noise_TherapyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
